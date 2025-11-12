90a615f6282d95d71ec4eee3c51da746c21f4eda
Micheli <michelimelki@gmail.com>
2025-11-12 19:29:52 -0300

    feat(work): cria e estiliza seção My Work com grid de projetos, hover e botão Show More

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Service from "./components/Service";
+import Work from "./components/Work";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Header />
       <About />
       <Service />
+      <Work />
     </>
   );
 }
