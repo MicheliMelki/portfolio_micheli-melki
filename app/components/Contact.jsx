f57216cac7b27a81e416df68db0f13ec95bfc291
Micheli <michelimelki@gmail.com>
2025-11-15 18:01:48 -0300

    feat(dark-mode): adiciona lógica global de tema e aplica estilização dark em todos os componentes

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 import Image from "next/image";
 import { assets } from "@/assets/assets";
 
-
 const Contact = () => {
   const [result, setResult] = useState("");
 
@@ -24,7 +23,8 @@ const Contact = () => {
   return (
     <div
       id="contact"
-      className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-size-[90%_auto]'
+      className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center
+      bg-size-[90%_auto] dark:bg-none'
     >
       <h4 className="text-center mb-2 text-lg font-Ovo">Conecte-se comigo</h4>
       <h2 className="text-center text-5xl font-Ovo">Entre em contato</h2>
@@ -41,25 +41,33 @@ const Contact = () => {
             type="text"
             placeholder="Digite o seu nome"
             required
-            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white" name="name"
+            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white 
+            dark:bg-darkHover/30 dark:border-white/90"
+            name="name"
           />
           <input
             type="email"
             placeholder="Digite o seu e-mail"
             required
-            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white"  name="email"
+            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white
+            dark:bg-darkHover/30 dark:border-white/90"
+            name="email"
           />
         </div>
         <textarea
           rows="6"
           placeholder="Digite sua mensagem!"
           required
-          className="w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6"  name="message"
+          className="w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6
+          dark:bg-darkHover/30 dark:border-white/90"
+          name="message"
         ></textarea>
 
         <button
           type="submit"
-          className="py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 cursor-pointer"
+          className="py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto 
+          hover:bg-black duration-500 cursor-pointer
+          dark:bg-transparent dark:border-[0.5px] dark:hover:darkHover"
         >
           Enviar!
           <Image src={assets.right_arrow_white} alt="" className="w-4" />
