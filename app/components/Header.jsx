eb858c19a8168af18ed13c06ea6dd548169ff35d
Micheli <michelimelki@gmail.com>
2025-11-15 19:36:48 -0300

    feat(animations): implementa framer-motion e adiciona animações em Header, About, Services, Work e Contact

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,41 +1,69 @@
 import { assets } from "@/assets/assets";
 import Image from "next/image";
 import React from "react";
+import { motion } from "framer-motion";
 
 const Header = () => {
   return (
     <div className="w-11/12 max-w-3x1 text-center mx-auto h-screen flex flex-col items-center justify-center gap-4">
-      <div>
+      <motion.div
+        initial={{ scale: 0 }}
+        whileInView={{ scale: 1 }}
+        transition={{ duration: 0.8, type: "spring", stiffness: 100 }}
+      >
         <Image src={assets.profile_img} alt="" className="rounded-full w-32" />
-      </div>
-      <h3 className="flex itemns-end gap-2 text-x1 md:text-2x1 mb-3 font-Ovo">
+      </motion.div>
+      <motion.h3
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.6, delay: 0.3 }}
+        className="flex itemns-end gap-2 text-x1 md:text-2x1 mb-3 font-Ovo"
+      >
         Oi! Eu sou a Micheli Melki{" "}
         <Image src={assets.hand_icon} alt="" className="w-6" />
-      </h3>
-      <h1 className="text-3x1 sm:text-6x1 lg:text-[66px]">
+      </motion.h3>
+      <motion.h1
+        initial={{ y: -30, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.8, delay: 0.5 }}
+        className="text-3x1 sm:text-6x1 lg:text-[66px]"
+      >
         FullStack Junior em JavaScript.
-      </h1>
-      <p className="max-w-2x1 mx-auto font-Ovo">
+      </motion.h1>
+
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.6, delay: 0.7 }}
+        className="max-w-2x1 mx-auto font-Ovo"
+      >
         Futura desenvolvedora em início de carreira, motivada e focada em
         aprendizado contínuo, buscando experiência prática em projetos de
         tecnologia.
-      </p>
+      </motion.p>
       <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
-        <a
+        <motion.a
+          initial={{ y: 30, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.6, delay: 1 }}
           href="#contact"
           className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent"
         >
           Entre em contato{" "}
           <Image src={assets.right_arrow_white} alt="" className="w-4" />
-        </a>
-        <a
+        </motion.a>
+
+        <motion.a
+          initial={{ y: 30, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.6, delay: 1.2}}
           href="#/sample-resume.pdf"
           download
           className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black"
         >
           Um pouco sobre mim{" "}
           <Image src={assets.download_icon} alt="" className="w-4" />
-        </a>
+        </motion.a>
       </div>
     </div>
   );
--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -1,21 +1,55 @@
 import { assets, workData } from "@/assets/assets";
 import Image from "next/image";
 import React from "react";
+import { motion } from "framer-motion";
 
 const Work = ({ isDarkMode }) => {
   return (
-    <div id="work" className="w-full px-[12%] py-10 scroll-mt-20">
-      <h4 className="text-center mb-2 text-lg font-Ovo">Meu portfolio</h4>
-      <h2 className="text-center text-5xl font-Ovo">Meus trabalhos</h2>
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      id="work"
+      className="w-full px-[12%] py-10 scroll-mt-20"
+    >
+      <motion.h4
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.3, duration: 0.5 }}
+        className="text-center mb-2 text-lg font-Ovo"
+      >
+        Meu portfolio
+      </motion.h4>
 
-      <p className="text-center max-w-2x1 mx-auto mt-5 mb-12 font-Ovo">
+      <motion.h2
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.5, duration: 0.5 }}
+        className="text-center text-5xl font-Ovo"
+      >
+        Meus trabalhos
+      </motion.h2>
+
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 0.7, duration: 0.5 }}
+        className="text-center max-w-2x1 mx-auto mt-5 mb-12 font-Ovo"
+      >
         Este é o meu portfólio, aqui você pode conhecer um pouco mais sobre meu
         trabalho, projetos e trajetória na área de tecnologia.
-      </p>
+      </motion.p>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 my-10 gap-5 dark:text-black">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 0.9, duration: 0.6 }}
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 my-10 gap-5 dark:text-black"
+      >
         {workData.map((project, index) => (
-          <div
+          <motion.div
+            whileHover={{ scale: 1.05 }}
+            transition={{ duration: 0.3 }}
             key={index}
             className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group"
             style={{ backgroundImage: `url(${project.bgImage})` }}
@@ -36,11 +70,14 @@ const Work = ({ isDarkMode }) => {
                 <Image src={assets.send_icon} alt="send icon" className="w-5" />
               </div>
             </div>
-          </div>
+          </motion.div>
         ))}
-      </div>
+      </motion.div>
 
-      <a
+      <motion.a
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 1.1, duration: 0.5 }}
         href=""
         className="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px]
        border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500 dark:text-white dark:border-white 
@@ -54,8 +91,8 @@ const Work = ({ isDarkMode }) => {
           alt="Right arrow"
           className="w-4"
         />
-      </a>
-    </div>
+      </motion.a>
+    </motion.div>
   );
 };
 
