a1897beddb4cdd33a4ae589d33bf439650675d6f
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2025-11-16 20:53:17 -0300

    feat: add text

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div>
-      
+    <div id="services" className='w-full px-[12%] py-10 scroll-mt-20'>
+      <h4 className='text-center mb-2 text-lg font-Ovo'>
+        What I offer
+      </h4>
+
+      <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+        I am a frontend developer from California, USA with 10 years of experience in multiple companies like Microsoft, Tesla and Apple.
+      </p>
     </div>
   )
 }
