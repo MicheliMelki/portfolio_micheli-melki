408716d6169ab6734bde20e5d45c572449b1ea45
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2025-11-16 13:47:38 -0300

    fix: reboot next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://192.168.68.106:3000',
+  ]
 };
 
 export default nextConfig;
