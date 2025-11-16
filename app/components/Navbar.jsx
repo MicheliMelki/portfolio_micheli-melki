9a82483c18e186441dd84e7bcd5c2d639d4d2f5e
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2025-11-16 20:30:09 -0300

    feat: effect on scroll

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,10 +1,11 @@
 'use client';
 import Image from 'next/image';
 import {assets} from '@/assets/assets';
-import React, {useRef} from 'react';
+import React, {useRef, useEffect, useState, use} from 'react';
 
 const Navbar = () => {
 
+  const [isScroll, setIsScroll] = useState(false);
   const sideMenuRef = useRef();
 
   const openMenu = () => {
@@ -15,17 +16,27 @@ const Navbar = () => {
     sideMenuRef.current.style.transform = 'translateX(16rem)';
   }
 
+useEffect(() => {
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      setIsScroll(true);
+    } else {
+      setIsScroll(false);
+    }
+  })
+}, []);
+
   return (
     <>
     <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
       <Image src={assets.header_bg_color} alt='' className='w-full' priority quality={95}/>
     </div>
-      <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
+      <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? 'bg-white bg-opacity-50 shadow-sm backdrop-blur-md' : ''} transition duration-500`}>
         <a href="#top">
           <Image src={assets.logo} alt="Logo" className="w-28 cursor-pointer mr-14" priority quality={95} />
         </a>
 
-        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
+        <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : "bg-white shadow-sm bg-opacity-50"} transition duration-500`}>
           <li><a className="font-Ovo" href="#top">Home</a></li>
           <li><a className="font-Ovo" href="#about">About me</a></li>
           <li><a className="font-Ovo" href="#services">Services</a></li>
