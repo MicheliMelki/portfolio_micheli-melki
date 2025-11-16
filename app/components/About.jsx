a616bd6c70bf8fa6b772c1a25269455d8c483b86
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2025-11-16 18:55:18 -0300

    feat: add titles

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
+    <div id='about' className='w-full px-[12%] py-10 scroll-mt-20'>
       <h4>Introduction</h4>
+      <h2>About me</h2>
     </div>
   )
 }
