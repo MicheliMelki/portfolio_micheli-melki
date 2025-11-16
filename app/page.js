4c24cb562a54c8eb763319a2810a406d727f952f
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2025-11-16 01:51:18 -0300

    fix: reboot app/page.js

--- app/page.js
+++ app/page.js
@@ -1,49 +1,8 @@
-"use client";
-import { useEffect, useState } from "react";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import Service from "./components/Service";
-import Work from "./components/Work";
+import Image from 'next/image';
 
 export default function Home() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  // Rodar apenas 1 vez no carregamento
-  useEffect(() => {
-    if (
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
-      setIsDarkMode(true);
-    } else {
-      setIsDarkMode(false);
-    }
-  }, []);
-
-  // Salva tema quando muda
-  useEffect(() => {
-    if (isDarkMode) {
-      document.documentElement.classList.add("dark");
-      localStorage.theme = "dark";
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.theme = "";
-    }
-  }, [isDarkMode]);
-
   return (
     <>
-      <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-      <Header isDarkMode={isDarkMode} />
-      <About isDarkMode={isDarkMode} />
-      <Service isDarkMode={isDarkMode} />
-      <Work isDarkMode={isDarkMode} />
-      <Contact isDarkMode={isDarkMode} />
-      <Footer isDarkMode={isDarkMode} />
     </>
   );
 }
