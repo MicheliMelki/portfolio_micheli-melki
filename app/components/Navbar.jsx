a1c7937ea2e27e93d104106934bb88d64cdf09bb
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2025-11-16 13:54:24 -0300

    fix: removido warning de carregamento de imagens

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -7,19 +7,19 @@ const Navbar = () => {
     <>
       <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
         <a href="#top">
-          <Image src={assets.logo} alt="Logo" className="w-28 cursor-pointer mr-14"/>
+          <Image src={assets.logo} alt="Logo" className="w-28 cursor-pointer mr-14" priority quality={95} />
         </a>
 
         <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3'>
-          <li><a href="#top">Home</a></li>
-          <li><a href="#about">About me</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#work">My Work</a></li>
-          <li><a href="#contact">Contact me</a></li>
+          <li><a className="font-Ovo" href="#top">Home</a></li>
+          <li><a className="font-Ovo" href="#about">About me</a></li>
+          <li><a className="font-Ovo" href="#services">Services</a></li>
+          <li><a className="font-Ovo" href="#work">My Work</a></li>
+          <li><a className="font-Ovo" href="#contact">Contact me</a></li>
         </ul>
 
         <div>
-          <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4'>Contact <Image src={assets.arrow_icon} className='w-3' alt="Contact Icon" /></a>
+          <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo'>Contact <Image src={assets.arrow_icon} className='w-3' alt="Contact Icon" /></a>
         </div>
       </nav>
     </>
