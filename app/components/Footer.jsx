65358e10332d7d0f9e4ef53292ce797b0016447c
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2025-11-20 19:00:48 -0300

    fix: recreate Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,47 +1,29 @@
-import { assets } from "@/assets/assets";
-import React from "react";
-import Image from "next/image";
+import Image from 'next/image'
+import {assets} from '@/assets/assets'
+import React from 'react'
 
-const Footer = ({ isDarkMode }) => {
+const Footer = () => {
   return (
-    <div className="mt-20">
-      <div className="text-center">
-        <Image
-          src={isDarkMode ? assets.logo_dark : assets.logo}
-          alt=""
-          className="w-36 mx-auto mb-2"
-        />
+    <div className='mt-20'>
+      <div className='text-center'>
+        <Image src={assets.logo} alt='' className='w-36 mx-auto mb-2' />
 
-        <div className="w-max flex items-center gap-2 mx-auto">
-          <Image
-            src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon}
-            alt=""
-            className="w-6"
-          />
-          michelimelki@gmail.com
+        <div className='w-max flex items-center gap-2 mx-auto'>
+            <Image src={assets.mail_icon} alt='' className='w-6' />
+            greatstackdev@gmail.com
         </div>
       </div>
 
-      <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6">
-        <p>©2025 Micheli Melki.</p>
-        <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-0">
-          <li>
-            <a target="_blank" href="https://github.com/MicheliMelki">
-              GitHub
-            </a>
-          </li>
-          <li>
-            <a
-              target="_blank"
-              href="https://www.linkedin.com/in/micheli-martins/"
-            >
-              LinkedIn
-            </a>
-          </li>
+      <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mt-12 py-6 mx-[10%]'>
+        <p>&copy; 2025 Micheli Melki. All rights reserved.</p>
+        <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
+            <li><a target='_blank' href="#">GitHub</a></li>
+            <li><a target='_blank' href="#">Linkedin</a></li>
+            <li><a target='_blank' href="#">Instagram</a></li>
         </ul>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
